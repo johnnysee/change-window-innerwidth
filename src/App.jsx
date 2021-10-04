91c400e9cbc6a17c4d32f91773d34474215df66b
johnnysee <js.johnny.see@gmail.com>
2021-10-04 23:26:41 +0200

    Added some Semantic-UI styling and some more text

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Message, Rating } from "semantic-ui-react";
 
 const App = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleResize = () => {
-    setWindowWidth(window.innerWidth)
-  }
+    setWindowWidth(window.innerWidth);
+  };
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize)
-  }, [])
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <div>
-    <h1>Your inner window width is:</h1>
-      <h2>
-      {windowWidth}
-      </h2>
-    </div>
-  )
-}
+    <>
+      <Container>
+      <br/>
+        <Message>
+          <Message.Header>Remember That You Can</Message.Header>
+          <p>
+            Resize your window and your inner window width will update
+            dynamically. Just drag or push your window!
+          </p>
+          <p>{windowWidth} pixels</p>
+        </Message>
+        <Rating maxRating={5} defaultRating={5} icon="star" size="massive" />
+      </Container>
+    </>
+  );
+};
 
-export default App
+export default App;
